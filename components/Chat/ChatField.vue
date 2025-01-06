71f3bf2d46c1d7a23fe0f10a96a05b670cfc6d75
david140240 <dallakhverdyan@bk.ru>
2025-01-06 15:47:14 +0300

    changes

--- components/Chat/ChatField.vue
+++ components/Chat/ChatField.vue
@@ -1,296 +1,355 @@
 <script setup lang="ts">
-    import { ref } from "vue";
+import { ref } from 'vue';
 
-    const chatStore = useChatStore(),
-        tooltipStore = useTooltipStore(),
-        messagesBlock = useTemplateRef("messages-block"),
-        refBottom = ref(null),
-        data = reactive({
-            inputField: "",
-            editingMessage: "",
-            idMessage: NaN as number,
-            idTooltip: NaN as number,
-            sendTimeMessage: null as string | null,
-            dynamicHeight: NaN as number,
-        }),
-        calculated = {
-            messages: computed(() => chatStore.data.messages),
-            isEditMode: computed((): boolean => chatStore.data.isEditMode),
-            btnIcons: computed((): string => {
-                return chatStore.data.isEditMode ? "check" : "paper-plane";
-            }),
-        },
-        methods = {
-            sendMessage: async () => {
-                if (data.inputField.length) {
-                    data.sendTimeMessage = methods.formatingTime(new Date());
-                    await chatStore.methods.addMessage(data.inputField);
-                    data.inputField = "";
-                    methods.scrollToLastMessage();
-                }
-            },
-            apply: (id?: number) => {
-                chatStore.data.isEditMode
-                    ? methods.saveChanges(id as number)
-                    : methods.sendMessage();
-            },
-            scrollToLastMessage: () => {
-                const height = messagesBlock.value?.scrollHeight;
-                messagesBlock.value?.scrollTo(0, height);
-            },
-            formatingTime: (time: Date) => {
-                return time.toLocaleTimeString("en-GB", {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                });
-            },
-            editMode: (text: string, id: number) => {
-                chatStore.data.isEditMode = true;
-                tooltipStore.data.isTooltipVisible = false;
-                data.inputField = text;
-                data.idMessage = id;
-                data.editingMessage = text;
-                methods.scrollToLastMessage();
-            },
-            saveChanges: (id: number) => {
-                if (data.inputField.length) {
-                    chatStore.data.messages[id].text = data.inputField;
-                    chatStore.data.messages[id].isEdited = true;
-                    data.inputField = "";
-                    data.editingMessage = "";
-                    chatStore.data.isEditMode = false;
-                    tooltipStore.data.isTooltipVisible = false;
-                    data.idMessage = id;
-                }
-            },
-            cancelEditMode: () => {
-                data.inputField = "";
-                data.editingMessage = "";
-                chatStore.data.isEditMode = false;
-            },
-            calculateHeight: () => {
-                if (refBottom.value) {
-                    if (chatStore.data.isEditMode) {
-                        data.dynamicHeight = 800 - 145;
-                    } else {
-                        data.dynamicHeight = 705;
-                    }
-                } else {
-                    data.dynamicHeight = 705;
-                }
-                methods.scrollToLastMessage();
-            },
-        };
+const chatStore = useChatStore(),
+	tooltipStore = useTooltipStore(),
+	messagesBlock = useTemplateRef('messages-block'),
+	refBottom = ref(null),
+	data = reactive({
+		inputField: '',
+		editingMessage: '',
+		idMessage: NaN as number,
+		idTooltip: NaN as number,
+		messageText: '',
+		sendTimeMessage: null as string | null,
+		dynamicHeight: NaN as number,
+		heightContainer: NaN as number,
+	}),
+	calculated = {
+		messages: computed(() => chatStore.data.messages),
+		isEditMode: computed((): boolean => chatStore.data.isEditMode),
+		btnIcons: computed((): string => {
+			return chatStore.data.isEditMode ? 'check' : 'paper-plane';
+		}),
+	},
+	methods = {
+		context: (text, id) => {
+			data.messageText = text;
+			data.idTooltip = id;
+		},
+		sendMessage: async () => {
+			if (data.inputField.length) {
+				data.sendTimeMessage = methods.formatingTime(new Date());
+				await chatStore.methods.addMessage(data.inputField);
+				data.inputField = '';
+				methods.scrollToLastMessage();
+			}
+		},
+		apply: (id?: number) => {
+			chatStore.data.isEditMode
+				? methods.saveChanges(id as number)
+				: methods.sendMessage();
+		},
+		scrollToLastMessage: () => {
+			const height = messagesBlock.value?.scrollHeight;
+			messagesBlock.value?.scrollTo(0, height);
+		},
+		formatingTime: (time: Date) => {
+			return time.toLocaleTimeString('en-GB', {
+				hour: '2-digit',
+				minute: '2-digit',
+			});
+		},
+		editMode: () => {
+			chatStore.data.isEditMode = true;
+			tooltipStore.data.isTooltipVisible = false;
+			data.inputField = data.messageText;
+			data.idMessage = data.idTooltip;
+			data.editingMessage = data.messageText;
+			methods.scrollToLastMessage();
+		},
+		saveChanges: (id: number) => {
+			if (data.inputField.length) {
+				chatStore.data.messages[id].text = data.inputField;
+				chatStore.data.messages[id].isEdited = true;
+				data.inputField = '';
+				data.editingMessage = '';
+				chatStore.data.isEditMode = false;
+				tooltipStore.data.isTooltipVisible = false;
+				data.idMessage = id;
+			}
+		},
+		cancelEditMode: () => {
+			data.inputField = '';
+			data.editingMessage = '';
+			chatStore.data.isEditMode = false;
+		},
+		calculateHeight: () => {
+			if (refBottom.value) {
+				if (chatStore.data.isEditMode) {
+					data.dynamicHeight = 800 - 145;
+					data.heightContainer = 84;
+				} else {
+					data.dynamicHeight = 705;
+					data.heightContainer = 90;
+				}
+			} else {
+				data.heightContainer = 90;
+				data.dynamicHeight = 705;
+			}
+			methods.scrollToLastMessage();
+		},
+		deleteMessage: (id: number) => {
+			chatStore.data.messages.forEach((item: string, i: number) => {
+				i === id ? chatStore.data.messages.splice(i, 1) : false;
+			});
+			tooltipStore.data.isTooltipVisible = false;
+		},
+		copyText: () => {
+			navigator.clipboard.writeText(data.messageText);
+			tooltipStore.data.isTooltipVisible = false;
+		},
+	};
 
-    watch(
-        () => chatStore.data.isEditMode,
-        () => {
-            methods.calculateHeight();
-        }
-    );
+watch(
+	() => chatStore.data.isEditMode,
+	() => {
+		methods.calculateHeight();
+	}
+);
 
-    onMounted(() => {
-        methods.calculateHeight();
-    });
+onMounted(() => {
+	methods.calculateHeight();
+});
 </script>
 
 <template>
-    <div class="c-chat-field">
-        <div class="content">
-            <div
-                class="empty"
-                v-if="
-                    chatStore.data.messages?.length === 0 ||
-                    !chatStore.data.messages
-                "
-            >
-                {{
-                    !chatStore.data.messages
-                        ? "Выберите, с кем хотели бы пообщаться"
-                        : "Начните общение!"
-                }}
-            </div>
-            <div class="container" v-else>
-                <div
-                    class="messages-block"
-                    ref="messages-block"
-                    :style="{ 'max-height': `${data.dynamicHeight}px` }"
-                >
-                    <ChatMessage
-                        v-for="(item, index) in chatStore.data.messages"
-                        @edit="methods.editMode"
-                        @contextmenu="data.idTooltip = index"
-                        :key="index"
-                        :text="item.text"
-                        :edit-flag="item.isEdited"
-                        :id="index"
-                        :id-tooltip="data.idTooltip"
-                        :send-time="data.sendTimeMessage"
-                    />
-                </div>
-            </div>
-            <div
-                class="input-block"
-                ref="refBottom"
-                v-if="chatStore.data.messages"
-            >
-                <div>
-                    <div
-                        v-if="chatStore.data.isEditMode"
-                        class="editing-message-preview"
-                    >
-                        <span class="title"> Редактирование </span>
-                        <span class="editing-message">
-                            {{ data.editingMessage }}
-                        </span>
-                    </div>
-                    <input
-                        class="chat-input"
-                        placeholder="Введите сообщение..."
-                        v-model="data.inputField"
-                        @keyup.enter="methods.apply(data.idMessage)"
-                    />
-                </div>
-                <div
-                    :class="[
-                        'send-btn',
-                        { '--disabled': !data.inputField.length },
-                    ]"
-                    @click="methods.apply(data.idMessage)"
-                >
-                    <img :src="`${calculated.btnIcons.value}.svg`" />
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="c-chat-field">
+		<div class="content">
+			<div
+				class="empty"
+				v-if="chatStore.data.messages?.length === 0 || !chatStore.data.messages"
+			>
+				<span>
+					{{
+						!chatStore.data.messages
+							? 'Выберите, с кем хотели бы пообщаться'
+							: 'Начните общение!'
+					}}
+				</span>
+			</div>
+			<div
+				class="container"
+				:style="{ height: `${data.heightContainer}%` }"
+				v-else
+			>
+				<DropList
+					v-if="tooltipStore.data.isTooltipVisible"
+					ref="tooltipRef"
+					v-click-outside="() => (tooltipStore.data.isTooltipVisible = false)"
+					:right="100"
+					:bottomn="50"
+				>
+					<template #content>
+						<span class="option" @click="methods.editMode">Редактировать</span>
+						<span class="option" @click="methods.copyText"
+							>Копировать текст</span
+						>
+						<span
+							class="option --delete"
+							@click="methods.deleteMessage(data.idTooltip)"
+							>Удалить</span
+						>
+					</template>
+				</DropList>
+				<div
+					class="messages-block"
+					ref="messages-block"
+					:style="{ 'max-height': `${data.dynamicHeight}px` }"
+				>
+					<ChatMessage
+						v-for="(item, index) in chatStore.data.messages"
+						@contextmenu="methods.context(item.text, index, event)"
+						:key="index"
+						:text="item.text"
+						:edit-flag="item.isEdited"
+						:id="index"
+						:id-tooltip="data.idTooltip"
+						:send-time="data.sendTimeMessage"
+					/>
+				</div>
+			</div>
+			<div class="input-block" ref="refBottom" v-if="chatStore.data.messages">
+				<div>
+					<div v-if="chatStore.data.isEditMode" class="editing-message-preview">
+						<span class="title"> Редактирование </span>
+						<span class="editing-message">
+							{{ data.editingMessage }}
+						</span>
+					</div>
+					<input
+						class="chat-input"
+						placeholder="Введите сообщение..."
+						v-model="data.inputField"
+						@keyup.enter="methods.apply(data.idMessage)"
+					/>
+				</div>
+				<div
+					:class="['send-btn', { '--disabled': !data.inputField.length }]"
+					@click="methods.apply(data.idMessage)"
+				>
+					<img :src="`${calculated.btnIcons.value}.svg`" />
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style lang="scss" scoped>
-    .c-chat-field {
-        height: 100%;
-        background-color: rgb(239, 239, 239);
+.c-chat-field {
+	height: 100%;
+	background-color: rgb(239, 239, 239);
 
-        .content {
-            width: 100%;
-            height: 100%;
-            position: relative;
+	.content {
+		width: 100%;
+		height: 100%;
+		position: relative;
 
-            .empty {
-                width: 100%;
-                height: 100%;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                font-size: 20px;
-            }
+		.empty {
+			width: 100%;
+			height: 100%;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			font-size: 20px;
 
-            .container {
-                display: flex;
-                height: 90%;
-                align-items: end;
-                padding: 10px 0px 10px 10px;
-                box-sizing: border-box;
-                overflow: hidden;
+			& > span {
+				background-color: gray;
+				padding: 5px 10px;
+				border-radius: 10px;
+				color: white;
+			}
+		}
 
-                .messages-block {
-                    display: flex;
-                    flex-direction: column;
-                    scrollbar-width: thin;
-                    scrollbar-gutter: stable;
-                    overflow-y: auto;
-                    gap: 10px;
-                    width: 100%;
-                }
-            }
+		.container {
+			display: flex;
+			align-items: end;
+			padding: 10px;
+			box-sizing: border-box;
+			overflow: hidden;
 
-            .input-block {
-                display: flex;
-                align-items: end;
-                gap: 10px;
-                align-self: flex-end;
-                position: absolute;
-                width: 100%;
-                bottom: 15px;
-                padding: 10px;
-                box-sizing: border-box;
-                background-color: #e7e7e7;
+			.messages-block {
+				display: flex;
+				flex-direction: column;
+				scrollbar-width: thin;
+				scrollbar-gutter: stable;
+				overflow-y: auto;
+				gap: 10px;
+				width: 100%;
+			}
 
-                & > div {
-                    width: 100%;
+			.droplist {
+				.option {
+					transition: 0.2s ease-in-out;
+					padding: 5px;
+					border-radius: 5px;
+					box-sizing: border-box;
+					min-width: 100%;
+					width: 100%;
 
-                    .editing-message-preview {
-                        color: #6c6c6c;
-                        padding: 5px;
-                        font-family: "Courier New", Courier, monospace;
-                        border-left: 3px rgb(56, 176, 255) solid;
+					&:hover {
+						background-color: rgb(230, 230, 230);
+						cursor: default;
+					}
 
-                        .editing-message {
-                            -webkit-line-clamp: 1;
-                            line-clamp: 1;
-                            text-overflow: ellipsis;
-                            display: -webkit-box;
-                            -webkit-box-orient: vertical;
-                            overflow: hidden;
-                        }
+					&.--delete {
+						color: red;
 
-                        & > span {
-                            display: block;
-                        }
+						&:hover {
+							background-color: rgb(255, 210, 210);
+						}
+					}
+				}
+			}
+		}
 
-                        .title {
-                            color: rgb(56, 176, 255);
-                            font-weight: 700;
-                        }
-                    }
+		.input-block {
+			display: flex;
+			align-items: end;
+			gap: 10px;
+			align-self: flex-end;
+			position: absolute;
+			width: 100%;
+			bottom: 15px;
+			padding: 10px;
+			box-sizing: border-box;
+			background-color: #e7e7e7;
 
-                    .chat-input {
-                        font-size: 16px;
-                        font-family: "Lucida Sans", "Lucida Sans Regular",
-                            "Lucida Grande", "Lucida Sans Unicode", Geneva,
-                            Verdana, sans-serif;
-                        width: 100%;
-                        min-height: 50px;
-                        border-radius: 10px;
-                        box-sizing: border-box;
-                        border: 1px rgb(229, 229, 229) solid;
-                        padding: 5px 15px;
-                        outline: none;
-                        outline-offset: 0;
-                    }
-                }
-                .edit-accept-btn {
-                    width: 50px;
-                }
+			& > div {
+				width: 100%;
 
-                .send-btn {
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    width: 70px;
-                    height: 50px;
-                    background-color: rgb(191, 224, 255);
-                    border-radius: 15px;
-                    transition: 0.2s;
+				.editing-message-preview {
+					color: #6c6c6c;
+					padding: 5px;
+					font-family: 'Courier New', Courier, monospace;
+					border-left: 3px rgb(56, 176, 255) solid;
 
-                    &.--disabled {
-                        background-color: rgb(187, 187, 187);
+					.editing-message {
+						-webkit-line-clamp: 1;
+						line-clamp: 1;
+						text-overflow: ellipsis;
+						display: -webkit-box;
+						-webkit-box-orient: vertical;
+						overflow: hidden;
+					}
 
-                        &:hover {
-                            background-color: rgb(187, 187, 187);
-                            cursor: default;
-                        }
-                    }
+					& > span {
+						display: block;
+					}
 
-                    &:hover {
-                        background-color: rgb(174, 216, 255);
-                        cursor: default;
-                    }
+					.title {
+						color: rgb(56, 176, 255);
+						font-weight: 700;
+					}
+				}
 
-                    &:active {
-                        background-color: rgb(144, 201, 255);
-                    }
-                }
-            }
-        }
-    }
+				.chat-input {
+					font-size: 16px;
+					font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande',
+						'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+					width: 100%;
+					min-height: 50px;
+					border-radius: 10px;
+					box-sizing: border-box;
+					border: 1px rgb(229, 229, 229) solid;
+					padding: 5px 15px;
+					outline: none;
+					outline-offset: 0;
+				}
+			}
+			.edit-accept-btn {
+				width: 50px;
+			}
+
+			.send-btn {
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				width: 70px;
+				height: 50px;
+				background-color: rgb(191, 224, 255);
+				border-radius: 15px;
+				transition: 0.2s;
+
+				&.--disabled {
+					background-color: rgb(187, 187, 187);
+
+					&:hover {
+						background-color: rgb(187, 187, 187);
+						cursor: default;
+					}
+				}
+
+				&:hover {
+					background-color: rgb(174, 216, 255);
+					cursor: default;
+				}
+
+				&:active {
+					background-color: rgb(144, 201, 255);
+				}
+			}
+		}
+	}
+}
 </style>
--- components/Chat/Message.vue
+++ components/Chat/Message.vue
@@ -1,174 +1,117 @@
 <script setup lang="ts">
-    const props = defineProps<{
-            text: string;
-            id: number;
-            idTooltip: number | null;
-            sendTime: string | null;
-            editFlag: boolean;
-        }>(),
-        emit = defineEmits<{
-            (e: "edit", text: string, id: number): void;
-        }>(),
-        messageRef = ref(),
-        tooltipRef = ref(),
-        chatStore = useChatStore(),
-        tooltipStore = useTooltipStore(),
-        data = reactive({
-            myTooltip: null as null | number,
-        }),
-        calculated = {
-            isEditMode: computed(() => chatStore.data.isEditMode),
-            isTooltipVisible: computed(
-                () =>
-                    tooltipStore.data.isTooltipVisible &&
-                    props.idTooltip === props.id
-            ),
-        },
-        methods = {
-            listener: () => {
-                const block = document.getElementById(props.id) as HTMLElement;
-                block.addEventListener("contextmenu", () => {
-                    tooltipStore.data.isTooltipVisible = true;
-                });
-            },
-            deleteMessage: (id: number) => {
-                chatStore.data.messages.forEach((item: string, i: number) => {
-                    i === id ? chatStore.data.messages.splice(i, 1) : false;
-                });
-                tooltipStore.data.isTooltipVisible = false;
-            },
-            copyText: () => {
-                navigator.clipboard.writeText(props.text);
-                tooltipStore.data.isTooltipVisible = false;
-            },
-        };
-    onMounted(() => {
-        methods.listener();
-    });
+const props = defineProps<{
+		text: string;
+		id: number;
+		idTooltip: number | null;
+		sendTime: string | null;
+		editFlag: boolean;
+	}>(),
+	emit = defineEmits<{
+		(e: 'edit', text: string, id: number): void;
+	}>(),
+	messageRef = ref(),
+	tooltipRef = ref(),
+	chatStore = useChatStore(),
+	tooltipStore = useTooltipStore(),
+	data = reactive({
+		myTooltip: null as null | number,
+	}),
+	calculated = {
+		isEditMode: computed(() => chatStore.data.isEditMode),
+		isTooltipVisible: computed(
+			() => tooltipStore.data.isTooltipVisible && props.idTooltip === props.id
+		),
+	},
+	methods = {
+		listener: () => {
+			const block = document.getElementById(props.id) as HTMLElement;
+			block.addEventListener('contextmenu', () => {
+				tooltipStore.data.isTooltipVisible = true;
+			});
+		},
+	};
+onMounted(() => {
+	methods.listener();
+});
 </script>
 
 <template>
-    <div
-        class="c-message"
-        @my-tooltip="(index: number) => (data.myTooltip = index)"
-    >
-        <DropList
-            v-if="calculated.isTooltipVisible.value"
-            ref="tooltipRef"
-            v-click-outside="() => (tooltipStore.data.isTooltipVisible = false)"
-            :right="15"
-            :bottomn="50"
-        >
-            <template #content>
-                <span class="option" @click="emit('edit', text, id)"
-                    >Редактировать</span
-                >
-                <span class="option" @click="methods.copyText"
-                    >Копировать текст</span
-                >
-                <span class="option --delete" @click="methods.deleteMessage(id)"
-                    >Удалить</span
-                >
-            </template>
-        </DropList>
-
-        <span
-            oncontextmenu="return false"
-            class="content"
-            :id="id"
-            ref="messageRef"
-        >
-            <span class="text">{{ text }}</span>
-            <div class="info">
-                <span class="edit-flag" v-if="editFlag">изменнено</span>
-                <span class="time">{{ sendTime }}</span>
-            </div>
-        </span>
-    </div>
+	<div
+		class="c-message"
+		@my-tooltip="(index: number) => (data.myTooltip = index)"
+	>
+		<span
+			oncontextmenu="return false"
+			class="content"
+			:id="id"
+			ref="messageRef"
+		>
+			<span class="text">{{ text }}</span>
+			<div class="info">
+				<span class="edit-flag" v-if="editFlag">изменнено</span>
+				<span class="time">{{ sendTime }}</span>
+			</div>
+		</span>
+	</div>
 </template>
 
 <style lang="scss" scoped>
-    .c-message {
-        position: relative;
-        display: flex;
-        flex-direction: column;
-        align-items: end;
-        gap: 5px;
-        padding: 0;
+.c-message {
+	position: relative;
+	display: flex;
+	flex-direction: column;
+	align-items: end;
+	gap: 5px;
+	padding: 0;
 
-        &.me {
-            justify-content: end;
-        }
+	&.me {
+		justify-content: end;
+	}
 
-        &.other {
-            justify-content: start;
-        }
+	&.other {
+		justify-content: start;
+	}
 
-        .content {
-            display: flex;
-            flex-direction: row;
-            align-items: end;
-            gap: 8px;
-            width: fit-content;
-            padding: 10px;
-            background-color: rgb(56, 176, 255);
-            color: white;
-            border-radius: 10px 10px 0 10px;
-            transition: 0.2s;
+	.content {
+		display: flex;
+		flex-direction: row;
+		align-items: end;
+		gap: 8px;
+		width: fit-content;
+		padding: 10px;
+		background-color: rgb(56, 176, 255);
+		color: white;
+		border-radius: 10px 10px 0 10px;
+		transition: 0.2s;
 
-            &:hover {
-                cursor: default;
-            }
+		&:hover {
+			cursor: default;
+		}
 
-            .text {
-                font-size: 16px;
-                color: white;
-                font-family: "Lucida Sans", "Lucida Sans Regular",
-                    "Lucida Grande", "Lucida Sans Unicode", Geneva, Verdana,
-                    sans-serif;
-            }
+		.text {
+			font-size: 16px;
+			color: white;
+			font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande',
+				'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+		}
 
-            .info {
-                display: flex;
-                gap: 7px;
-                width: min-content;
-                color: rgb(227, 227, 227);
+		.info {
+			display: flex;
+			gap: 7px;
+			width: min-content;
+			color: rgb(227, 227, 227);
 
-                .edit-flag {
-                    font-size: 13px;
-                }
+			.edit-flag {
+				font-size: 13px;
+			}
 
-                .time {
-                    width: min-content;
-                    font-size: 12px;
-                    font-family: "Gill Sans", "Gill Sans MT", Calibri,
-                        "Trebuchet MS", sans-serif;
-                }
-            }
-        }
-
-        .droplist {
-            .option {
-                transition: 0.2s ease-in-out;
-                padding: 5px;
-                border-radius: 5px;
-                box-sizing: border-box;
-                min-width: 100%;
-                width: 100%;
-
-                &:hover {
-                    background-color: rgb(230, 230, 230);
-                    cursor: default;
-                }
-
-                &.--delete {
-                    color: red;
-
-                    &:hover {
-                        background-color: rgb(255, 210, 210);
-                    }
-                }
-            }
-        }
-    }
+			.time {
+				width: min-content;
+				font-size: 12px;
+				font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS',
+					sans-serif;
+			}
+		}
+	}
+}
 </style>
